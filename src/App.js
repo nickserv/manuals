73f6bf51e73ec93da67ad70acc8122175ac00c2b
Nick McCurdy <nick@nickmccurdy.com>
2019-09-09 04:43:01 -0400

    Add Nintendo Switch Online button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ function App() {
   return (
     <>
       <h1>Manuals</h1>
+      <a
+        className="switch-online"
+        href="https://www.nintendo.com/switch/online-service/nes/"
+      >
+        Play these games on Nintendo Switch Online
+      </a>
       {Object.entries(manuals).map(([heading, games]) => (
         <Fragment key={heading}>
           <h2>{heading}</h2>
