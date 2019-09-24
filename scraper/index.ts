c44c13bf2cf0d696252a877db760a62599918207
Nick McCurdy <nick@nickmccurdy.com>
2019-09-24 09:05:20 -0400

    Finish normalizing data for English JSON file

--- scraper/index.ts
+++ scraper/index.ts
@@ -19,6 +19,46 @@ function sortGames(games: Game[]) {
   });
 }
 
+function titleCase(text: string) {
+  return text
+    .split(/(?<splitter> |-)/u)
+    .map(word => {
+      if (/^[IVXLCDM:]+$/iu.test(word)) {
+        return word.toUpperCase();
+      }
+      if (["of", "to", "the"].includes(word)) {
+        return word.toLowerCase();
+      }
+      // eslint-disable-next-line no-magic-numbers
+      return word[0].toUpperCase() + word.slice(1).toLowerCase();
+    })
+    .join("");
+}
+
+function normalizeGame(game: string) {
+  const normalizedGame = titleCase(game.trim())
+    .replace(/®|™/gu, "")
+    .replace(/’/gu, "'")
+    .replace("Bros ", "Bros. ");
+
+  switch (normalizedGame) {
+    case "Ghosts'n Goblins":
+      return "Ghosts 'n Goblins";
+    case "Startropics":
+      return "StarTropics";
+    case "Super Ghouls'n Ghosts":
+      return "Super Ghouls 'n Ghosts";
+    case "Yoshi's Island":
+      return "Super Mario World 2: Yoshi's Island";
+    default:
+      return normalizedGame;
+  }
+}
+
+function normalizeGames(games: Game[]) {
+  return games.map(game => ({ ...game, game: normalizeGame(game.game) }));
+}
+
 interface Scraper {
   url: string;
   execute(page: puppeteer.Page): Promise<Game[]>;
@@ -54,21 +94,42 @@ const scrapers: Record<string, Scraper> = {
 
 const gotoOptions = { waitUntil: "domcontentloaded" } as const;
 
-async function executeScraper(browser: puppeteer.Browser, scraper: Scraper) {
+async function executeScraper(
+  browser: puppeteer.Browser,
+  scraper: Scraper,
+  switchGames: string[]
+) {
   const page = await browser.newPage();
   await page.goto(scraper.url, gotoOptions);
-  return sortGames(await scraper.execute(page));
+  return sortGames(
+    normalizeGames(await scraper.execute(page)).filter(game =>
+      switchGames.includes(game.game)
+    )
+  );
 }
 
 async function executeScrapers(
   browser: puppeteer.Browser
 ): Promise<Record<string, Game[]>> {
+  const page = await browser.newPage();
+  await page.goto(
+    "https://www.nintendo.com/switch/online-service/nes/",
+    gotoOptions
+  );
+  const switchGames = (await page.evaluate(() =>
+    Array.from(document.getElementsByClassName("content-copy")).flatMap(node =>
+      Array.from(node.childNodes)
+        .map(childNode => childNode.textContent!)
+        .filter(text => /\w/u.test(text))
+    )
+  )).map(normalizeGame);
+
   return Object.fromEntries(
     await Promise.all(
       Object.entries(scrapers).map(
         async ([consoleName, scraper]): Promise<[string, Game[]]> => [
           consoleName,
-          await executeScraper(browser, scraper)
+          await executeScraper(browser, scraper, switchGames)
         ]
       )
     )
